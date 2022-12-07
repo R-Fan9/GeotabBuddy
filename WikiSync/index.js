1d391fda03fa7d13b161c868e79af3d9da276340
Vishwesh Patel <vishweshpatel@geotab.com>
2022-12-07 15:58:32 -0500

    cleanup

--- WikiSync/index.js
+++ WikiSync/index.js
@@ -2,6 +2,7 @@ const { google } = require("googleapis");
 const { PubSub } = require("@google-cloud/pubsub");
 const { getAllDocIds, getDocText, syncDocToES } = require("./docESsync");
 const { parseTextToJson } = require("./utils");
+const elasticClient = require("./elastic-client");
 
 const scopes = ["https://www.googleapis.com/auth/drive", "https://www.googleapis.com/auth/documents"];
 
@@ -47,3 +48,57 @@ exports.docElasticSearchSync = async (req, res) => {
   }
 };
 
+exports.updateFAQDoc = async (eventData, context, callback) => {
+  const googleDoc = google.docs({ version: 'v1', auth });
+  const doc = await googleDoc.documents.get({
+    documentId: '1Tnc4vvsMgxbsGQKdshsUmPWVhrz2MXbQMCl75NTeXlM',
+    fields: 'body/content'
+  });
+
+  const endIndex = doc.data.body.content.at(-1).endIndex;
+  
+  const res = await elasticClient.search({
+    index: "questions",
+    query: {
+      match_all: {}
+    },
+  });
+
+  const respBody = res.hits.hits;
+
+  let docContent = "FAQ Questions\n\n";
+  respBody.forEach(entry => {
+    const question = entry._source.question;
+    const answer = entry._source.answer;
+    
+    docContent += question + "\n";
+    docContent += answer + "\n";
+    docContent += "\n";
+    
+  });
+
+  await googleDoc.documents.batchUpdate({
+    documentId: '1Tnc4vvsMgxbsGQKdshsUmPWVhrz2MXbQMCl75NTeXlM',
+    resource: {
+      requests: [
+        {
+          "deleteContentRange": {
+            "range": {
+              "startIndex": 1,
+              "endIndex": endIndex - 1
+            }
+          }
+        },
+        {
+          "insertText": {
+            "text": docContent.toString(),
+            "endOfSegmentLocation": {
+              "segmentId": ""
+            }
+          }
+        }
+      ]
+    }
+  });
+
+}
